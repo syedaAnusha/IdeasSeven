d5e72b6f1209830c5bc83c5d9eb57b5adfe0ad6f
Anusha Syeda <anoshasyeda1999@gmail.com>
2024-02-16 21:48:38 +0500

    modified script.js

---
+++ Script.js
@@ -0,0 +1,15 @@
+function goToSecondPage() {
+  window.location.href = "./secondScreen.html";
+}
+
+function goToThirdPage() {
+  window.location.href = "./thirdScreen.html";
+}
+
+function goToFourthPage() {
+  window.location.href = "./fourthScreen.html";
+}
+
+function goToLastPage() {
+  window.location.href = "./lastScreen.html";
+}
