04232656fb4adbb8aa4d9bea8996803957384757
Anusha Syeda <anoshasyeda1999@gmail.com>
2024-02-16 21:12:37 +0500

    completed: Second page with responsiveness

---
+++ Javascript files/Script.js
@@ -0,0 +1,7 @@
+function goToSecondPage() {
+  window.location.href = "../Html Files/secondScreen.html";
+}
+
+function goToThirdPage() {
+  window.location.href = "../Html Files/thirdScreen.html";
+}
